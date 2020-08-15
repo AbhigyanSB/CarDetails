f3cf84ee7072b29a02debc179b0818b860cf6cf4
Abhigyan Sarma Barua <sarmabaruaabhigyan@gmail.com>
2020-08-16 00:03:21 +0100

    Add files via upload

---
+++ PRSI.java
@@ -0,0 +1,7 @@
+package sarmabaruaabhigyan_OOP_05;
+
+//The PRSI interface
+public interface PRSI {
+	static final double rate = 0.04; 		//As 4% = 0.04
+	public abstract double calculatePRSI();		//Abstract method
+}
