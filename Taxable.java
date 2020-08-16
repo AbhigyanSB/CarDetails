3f1be5bff2ac883e6e76dda194e2e29e4dd8c61b
Abhigyan Sarma Barua <sarmabaruaabhigyan@gmail.com>
2020-08-17 00:22:37 +0100

    Add files via upload

---
+++ Taxable.java
@@ -0,0 +1,7 @@
+package sarmabaruaabhigyan_OOP_05;
+
+//The taxable interface
+public interface Taxable {
+	public abstract double calculateTax();		//Method to calculate tax
+	public abstract void printTax();		//Method to print tax
+}
